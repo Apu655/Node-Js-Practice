b3729b63ca4f78eaa0881d495a0a0bd572344400
unknown <apu.islam@g.bracu.ac.bd>
2022-10-24 19:21:42 +0600

    practice 1 update

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`,"utf-8")
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`,"utf-8vb ")
 ourReadStream.on("data",(chunk)=>{
     console.log(chunk)
 })
